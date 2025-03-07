8b89eda1f7ba63f0fda7232e0a0b9a9242153ee0
IshanOrdex <ishan.trivedi@ordextechnology.com>
2025-03-07 19:35:45 +0530

    Initial Commit

---
+++ bookcrudsystem/src/main/java/com/aicrud/bookcrudsystem/BookcrudsystemApplication.java
@@ -0,0 +1,13 @@
+package com.aicrud.bookcrudsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcrudsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcrudsystemApplication.class, args);
+	}
+
+}
---
+++ bookcrudsystem/src/test/java/com/aicrud/bookcrudsystem/BookcrudsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicrud.bookcrudsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcrudsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
